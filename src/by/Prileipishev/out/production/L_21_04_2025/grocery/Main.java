cb5264700cef76d92768b88cdca9bf00f838a6b5
vad0ss <gr.west20@gmail.com>
2025-04-23 14:54:38 +0300

    Streams Home task

--- src/by/Prileipishev/out/production/L_21_04_2025/grocery/Main.java
+++ src/by/Prileipishev/out/production/L_21_04_2025/grocery/Main.java
@@ -1,14 +1,11 @@
 package by.Prileipishev.out.production.L_21_04_2025.grocery;
 
-import by.Prileipishev.out.production.L_21_04_2025.Dish;
-import by.Prileipishev.out.production.L_21_04_2025.Type;
 import by.Prileipishev.out.production.L_21_04_2025.grocery.factory.GroceryItemFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class Main {
 
@@ -39,11 +36,49 @@ public class Main {
                 .sorted(Comparator.comparing(GroceryItem::getPrice))
                 .toList();
 
+        List<GroceryItem> decrasePriceSorted = groceryItems.stream()
+                .sorted(Comparator.comparing(GroceryItem::getPrice).reversed())
+                .limit(3)
+                .toList();
+
+        List<String> topThreeNames = decrasePriceSorted.stream()
+                .map(GroceryItem::getName)
+                .toList();
+
+
         System.out.println(dairy);
         System.out.println(perishable);
         System.out.println(priceSorted);
+        System.out.println(decrasePriceSorted);
+        System.out.println(topThreeNames);
 
+        groceryItems.forEach(item -> System.out.println(item.getName() + ": price * " + item.getPrice() + " Br"));
+
+        boolean cheap = groceryItems.stream().anyMatch(item -> item.getPrice() < 1);
+        boolean noZero = groceryItems.stream().allMatch(item -> item.getPrice() <= 0);
+
+        if (cheap) {
+            System.out.println("Товар дешевле 1 рубля есть");
+        } else {
+            System.out.println("Товара дешевле 1 рубля нет");
+        }
+
+        if (noZero) {
+            System.out.println("Товар равных или меньше 0 нет");
+        } else {
+            System.out.println("Товары дешевле или равные 0 есть");
+        }
+
+        Optional<GroceryItem> opFirstVeg = groceryItems.stream().filter(item -> item.getCategory() == Category.VEGETABLE).findFirst();
+
+        opFirstVeg.ifPresent(item -> System.out.println(item.getName()));
+
+        List<Category> uniqueCategories = groceryItems.stream()
+                .map(GroceryItem::getCategory)
+                .distinct()
+                .toList();
+
+        System.out.println(uniqueCategories);
     }
-
 
 }
--- src/by/Prileipishev/out/production/L_21_04_2025/grocery/factory/GroceryItemFactory.java
+++ src/by/Prileipishev/out/production/L_21_04_2025/grocery/factory/GroceryItemFactory.java
@@ -12,8 +12,8 @@ public class GroceryItemFactory {
     private static final Random random = new Random();
 
 
-    private List<String> categoryNames = List.of("DAIRY", "VEGETABLE", "FRUIT", "BAKERY", "MEAT", "BEVERAGE");
-    private Map<Category, List<String>> map = Map.of(
+    private final List<String> categoryNames = List.of("DAIRY", "VEGETABLE", "FRUIT", "BAKERY", "MEAT", "BEVERAGE");
+    private final Map<Category, List<String>> map = Map.of(
             Category.DAIRY, List.of("молоко", "сметана"),
             Category.VEGETABLE, List.of("салат", "сельдерей"),
             Category.FRUIT, List.of("апельсин", "банан"),
@@ -23,11 +23,11 @@ public class GroceryItemFactory {
     );
 
     public GroceryItem next() {
-        Category category = Category.valueOf(categoryNames.get(random.nextInt(0, 2)));
+        Category category = Category.valueOf(categoryNames.get(random.nextInt(0, 6)));
         List<String> names = map.get(category);
         String name = names.get(random.nextInt(0,2));
 
-        return new GroceryItem(name, category, random.nextDouble(1,100), random.nextBoolean());
+        return new GroceryItem(name, category, random.nextDouble(0,20), random.nextBoolean());
     }
 
 
