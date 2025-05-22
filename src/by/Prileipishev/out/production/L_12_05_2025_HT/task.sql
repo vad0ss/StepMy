4a3f22c5355605169bd4a67ebbffd84b4e507daa
vadim prilepishev <gr.west20@gmail.com>
2025-05-22 21:03:37 +0300

    SQL tasks

--- src/by/Prileipishev/out/production/L_12_05_2025_HT/task.sql
+++ src/by/Prileipishev/out/production/L_12_05_2025_HT/task.sql
@@ -102,3 +102,6 @@ SELECT DISTINCT category FROM books WHERE price BETWEEN 10 AND 50;
 
 
 SELECT u.fullname as FIO, COUNT(o.total) AS Counter FROM users u INNER JOIN orders o ON u.id = o.user_id GROUP BY u.fullname;
+
+
+SELECT country.country_name, country.capital AS country FROM countries LEFT JOIN continents AS continent ON country.continent_id=continent.continent_id;
